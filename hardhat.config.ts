216e5591103a7ff964f9f165eac74360d42a8d34
Cancu <juancruz.cancu@gmail.com>
2022-10-08 13:26:14 -0300

    init repo - brokes

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+};
+
+export default config;
